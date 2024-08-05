3f432f2a92d85d88d8ed573500d5ee40f1d2464d
chaarliee <ceupena413@gmail.com>
2024-08-05 21:59:36 +0800

    Set up initial code structure
    - Set up app routing module for handling navigation
    - Create core components: Shared and Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { StreamComponent } from './pages/stream/stream.component';
+import { UploadComponent } from './pages/upload/upload.component';
+
+const routes: Routes = [
+  { path: '',component: HomeComponent },
+  { path: 'stream', component: StreamComponent },
+  { path: 'upload', component: UploadComponent },
+
+  // { path: 'error', component: ErrorComponent, },
+  // { path: '**', component: NotFoundComponent }
+ 
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  // imports: [RouterModule.forRoot(routes)],
+  imports: [
+      RouterModule.forRoot(routes, {
+          preloadingStrategy: PreloadAllModules, // <- comment this line for activate lazy load
+          relativeLinkResolution: 'legacy',
+          // useHash: true
+      })
+  ],
+  exports: [
+      RouterModule
+  ]
+
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StreamComponent } from './pages/stream/stream.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StreamComponent,
+    HomeComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
